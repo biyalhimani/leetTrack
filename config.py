c47d1219791afb69bc805e10f6905bff47eced93
biyalhimani <biyalhimani@gmail.com>
2026-07-10 00:15:59 +0530

    Prepare application for deployment

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+import os
+
+
 class Config:
-    SECRET_KEY = "mysecretkey123"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:bazzinga@localhost:5432/leettrack"
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkey123")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "postgresql://postgres:bazzinga@localhost:5432/leettrack"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
